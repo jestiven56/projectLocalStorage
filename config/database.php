c4707a77dfb3bfb54dc81a85564c92dfa7468304
jestiven56 <jestiven56@gmail.com>
2025-03-14 19:06:39 -0500

    fix puerto deploy test

--- config/database.php
+++ config/database.php
@@ -4,10 +4,12 @@ define('DB_HOST', 'ovh1.clusters.zeabur.com');
 define('DB_USER', 'root');
 define('DB_PASS', 'rgxbP3jsMo2657f8q9T4YUAhelaI10wk');
 define('DB_NAME', 'zeabur');
+define('DB_PORT', 31833);
+
 
 // Función para obtener la conexión a la base de datos
 function getDbConnection() {
-    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
     
     // Verificar conexión
     if ($conn->connect_error) {
